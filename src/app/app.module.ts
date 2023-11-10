21961cd2dac6f721de5302d8244ca3df50ec6fdf
angtdiaz <nicoltuarezd@gmail.com>
2023-11-09 22:20:27 -0500

    feat: firebase config implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"digimon-back","appId":"1:989691601532:web:f6421be74addae7d499ff9","storageBucket":"digimon-back.appspot.com","apiKey":"AIzaSyBplPJFmP1ry3pDbNrbT03Or65sps5L-n4","authDomain":"digimon-back.firebaseapp.com","messagingSenderId":"989691601532"})),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
