aab9cd139346df1358d904b1610ae90d32d448ec
angtdiaz <nicoltuarezd@gmail.com>
2023-11-11 11:40:46 -0500

    feat: index, style and app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthService } from './core/services/auth.service';
+import { SharedModule } from './shared/shared.module';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { provideIcons } from './core/icons/icons.provider';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptorService } from './core/interceptors/auth.interceptor';
+import { LayoutModule } from './layout/layout.module';
+import { HttpService } from './core/services/http.service';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,8 +25,23 @@ import { AuthService } from './core/services/auth.service';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     BrowserAnimationsModule,
+    HttpClientModule,
+    LayoutModule,
+    InfiniteScrollModule,
+    SharedModule,
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    HttpService,
+    provideIcons(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: true } },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
